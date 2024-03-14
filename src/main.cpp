b9acb9fe147ca01a21c80bc8832896941cd68c56
cyril grinko <k.a.grinko@gmail.com>
2024-03-14 15:48:01 +0300

    main ready for 1 iteration

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,29 @@
 #include "Simulation.h"
 #include "GraphicsComponent.h"
 
-int main() {
+#include <QApplication>
+#include <QWidget>
+
+int main(int argc, char* argv[]) {
+  // QApplication app(argc, argv);
+
+  // QWidget window;
+  // window.setWindowTitle("Molecules Simulation");
+  
   Box box(200, 200);
-
   std::vector<Particle> gas;
-
-  for (int i = 0; i < 10; i++) {
-    for (int j = 0; j < 10; j++) {
-      Particle molecule(Vector(10 * i, 10 * j), Vector(1, 1), 1, 1);
-      gas.push_back(molecule);
-    }
-  }
-
   Simulation simulation(gas, box);
   GraphicsComponent graphics;
+
+  simulation.gas.emplace_back(Vector(90, 100), Vector(10, 0), 1, 1);
+  simulation.gas.emplace_back(Vector(100, 100), Vector(-10, 0), 1, 1);
   
   for (size_t i = 0; i < 1000; i++) {
     graphics.Render(simulation.gas, box);
     simulation.ManageCollisions();
     simulation.MoveParticles(0.001);
   }
-  return 0;
+
+  // window.show();
+  // return app.exec();
 }
