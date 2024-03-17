c2a3302320c3f2ba4169fe0eb599aa0b1098d50d
cyril grinko <k.a.grinko@gmail.com>
2024-03-17 14:30:57 +0300

    add magic constants file

---
+++ include/constants.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+
+const double TOLERANCE = 1e-2;
+
+const Vector LEFT_WALL_NORMAL = Vector(1, 0);
+
+const Vector RIGHT_WALL_NORMAL = Vector(-1, 0);
+
+const Vector TOP_WALL_NORMAL = Vector(0, -1);
+
+const Vector BOTTOM_WALL_NORMAL = Vector(0, 1);
