59c353f9ff2195d8824e367ad26d76cfe0acc725
cyril grinko <k.a.grinko@gmail.com>
2024-03-17 14:32:14 +0300

    added magic constants

--- src/Box.cpp
+++ src/Box.cpp
@@ -1,26 +1,27 @@
 #include "Box.h"
+#include "constants.h"
 
-Box::Box(int width, int height) : width(width), height(height) {
+Box::Box(int width, int height) : width_(width), height_(height) {
 }
 
 Vector Box::GetNormal(const Vector& pos) const {
-  if (pos.x <= width / 2 && pos.x <= pos.y && pos.x <= (height - pos.y)) {
-    return Vector(1, 0);
+  if (pos.x_ <= width_ / 2 && pos.x_ <= pos.y_ && pos.x_ <= (height_ - pos.y_)) {
+    return LEFT_WALL_NORMAL;
   }
-  else if (pos.x >= width / 2 && (width - pos.x) <= pos.y && (width - pos.x) <= (height - pos.y)) {
-    return Vector(-1, 0);
+  else if (pos.x_ >= width_ / 2 && (width_ - pos.x_) <= pos.y_ && (width_ - pos.x_) <= (height_ - pos.y_)) {
+    return RIGHT_WALL_NORMAL;
   }
-  else if (pos.y <= height / 2 && pos.y <= pos.x && pos.y <= (width - pos.x)) {
-    return Vector(0, 1);
+  else if (pos.y_ <= height_ / 2 && pos.y_ <= pos.x_ && pos.y_ <= (width_ - pos.x_)) {
+    return BOTTOM_WALL_NORMAL;
   }
-  else if (pos.y >= height / 2 && (height - pos.y) <= pos.x && (height - pos.y) <= (width - pos.x)) {
-    return Vector(0, -1);
+  else if (pos.y_ >= height_ / 2 && (height_ - pos.y_) <= pos.x_ && (height_ - pos.y_) <= (width_ - pos.x_)) {
+    return TOP_WALL_NORMAL;
   }
-  return Vector(0, -1);
+  return TOP_WALL_NORMAL;
 }
 
 bool Box::CheckCollision(const Particle& particle) {
-  double tolerance = particle.radius + 1e-2;
-  return (particle.position.x < tolerance) || (particle.position.y < tolerance) ||
-         ((width - particle.position.x) < tolerance) || ((height - particle.position.x) < tolerance);
+  double gap = particle.radius_ + TOLERANCE;
+  return (particle.position_.x_ < gap) || (particle.position_.y_ < gap) ||
+         ((width_ - particle.position_.x_) < gap) || ((height_ - particle.position_.x_) < gap);
 };
--- src/Particle.cpp
+++ src/Particle.cpp
@@ -1,19 +1,20 @@
 #include "Particle.h"
+#include "constants.h"
 
 Particle::Particle(const Vector& position, const Vector& velocity,
-                   double radius, double mass) : position(position), velocity(velocity), radius(radius), mass(mass) {
+                   double radius, double mass) : position_(position), velocity_(velocity), radius_(radius), mass_(mass) {
 };
 
 Vector Particle::GetImpulse() {
-  return velocity * mass;
+  return velocity_ * mass_;
 };
 
 void Particle::UpdatePosition(double timeStep) {
-  position.x += velocity.x * timeStep;
-  position.y += velocity.y * timeStep;
+  position_.x_ += velocity_.x_ * timeStep;
+  position_.y_ += velocity_.y_ * timeStep;
 };
 
 bool Particle::CheckCollision(const Particle& molecule) {
-  double tolerance = 1e-2;
-  return (position - molecule.position).Length() - radius - molecule.radius < tolerance;
+  double gap = radius_ + molecule.radius_ + TOLERANCE;
+  return (position_ - molecule.position_).Length() < gap;
 };
