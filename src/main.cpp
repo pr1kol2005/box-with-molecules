ef706d4b15f93008b5a005b9b98b1a3616934ad5
cyril grinko <k.a.grinko@gmail.com>
2024-04-10 18:07:53 +0300

    main changed for showing window

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,27 @@
 #include "Simulation.h"
 #include "GraphicsComponent.h"
+#include "MainWidget.h"
 
 #include <QApplication>
-#include <QWidget>
 
 int main(int argc, char* argv[]) {
-  // QApplication app(argc, argv);
+  // Box box(200, 200);
+  // std::vector<Particle> gas;
+  // Simulation simulation(gas, box);
+  // GraphicsComponent graphics;
 
-  // QWidget window;
-  // window.setWindowTitle("Molecules Simulation");
+  // simulation.AddParticle(Particle(Vector(90, 90), Vector(10, 10), 1, 1));
+  // simulation.AddParticle(Particle(Vector(100, 100), Vector(-10, -10), 1, 1));
+  // simulation.AddParticle(Particle(Vector(195, 100), Vector(10, 0), 1, 1));
   
-  Box box(200, 200);
-  std::vector<Particle> gas;
-  Simulation simulation(gas, box);
-  GraphicsComponent graphics;
+  // for (size_t i = 0; i < 1000; i++) {
+  //   graphics.Render(simulation.GetGas(), box);
+  //   simulation.ManageCollisions();
+  //   simulation.MoveParticles(0.001);
+  // }
 
-  simulation.AddParticle(Particle(Vector(90, 90), Vector(10, 10), 1, 1));
-  simulation.AddParticle(Particle(Vector(100, 100), Vector(-10, -10), 1, 1));
-  simulation.AddParticle(Particle(Vector(195, 100), Vector(10, 0), 1, 1));
-  
-  for (size_t i = 0; i < 1000; i++) {
-    graphics.Render(simulation.GetGas(), box);
-    simulation.ManageCollisions();
-    simulation.MoveParticles(0.001);
-  }
-
-  // window.show();
-  // return app.exec();
+  QApplication a(argc, argv);
+  MainWidget w;
+  w.show();
+  return a.exec();
 }
