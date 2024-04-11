f35bda7b34bdf87264b64ee544d0160b7afdb40a
cyril grinko <k.a.grinko@gmail.com>
2024-04-11 13:44:47 +0300

    add new time related constants

--- include/constants.h
+++ include/constants.h
@@ -4,6 +4,10 @@
 
 const double TOLERANCE = 1e-2;
 
+const double FPS = 1000 / 100;
+
+const double TIME_INTERVAL = FPS / 1000;
+
 const Vector LEFT_WALL_NORMAL = Vector(1, 0);
 
 const Vector RIGHT_WALL_NORMAL = Vector(-1, 0);
