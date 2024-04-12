5ffebc6975a3a6a0d5f4da9eb136ea1b3664f8ea
cyril grinko <k.a.grinko@gmail.com>
2024-04-12 19:38:45 +0300

    changed spawn logic

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,14 @@ int main(int argc, char* argv[]) {
   Box box(BOX_WIDTH, BOX_HEIGHT);
   std::vector<Particle> gas;
   Simulation simulation(gas, box);
+  std::srand(std::time(0));
 
   for (int i = 0; i < PARTICLE_NUMBER; i++) {
     simulation.AddParticle(Particle(Vector(PARTICLE_SIZE + (1 + ( std::rand() % (BOX_WIDTH / (2 * PARTICLE_SIZE) - 1))) * 2 * PARTICLE_SIZE,
                                            PARTICLE_SIZE + (1 + ( std::rand() % (BOX_HEIGHT / (2 * PARTICLE_SIZE) - 1))) * 2 * PARTICLE_SIZE),
-                                    Vector(std::rand() % MAX_SPAWN_VELOCITY, std::rand() % MAX_SPAWN_VELOCITY), PARTICLE_SIZE, 1));
+                                    Vector(MAX_SPAWN_VELOCITY - std::rand() % (2 * MAX_SPAWN_VELOCITY),
+                                           MAX_SPAWN_VELOCITY - std::rand() % (2 * MAX_SPAWN_VELOCITY)),
+                                    PARTICLE_SIZE, 1));
   }
 
   QApplication a(argc, argv);
