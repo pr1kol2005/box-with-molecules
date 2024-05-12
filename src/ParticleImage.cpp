ccd1b959714a64a4c896a3d21c122c3ae8de83a4
cyril grinko <k.a.grinko@gmail.com>
2024-05-12 03:29:04 +0300

    color now depends on velocity

--- src/ParticleImage.cpp
+++ src/ParticleImage.cpp
@@ -1,7 +1,10 @@
 #include "ParticleImage.h"
 
+#include <iostream>
+
 ParticleImage::ParticleImage(Particle* molecule) : molecule_(molecule){
-  QColor color = QColor(255, 255, 0);
+  double heat_value = (molecule_->velocity_.Length()) / MAX_SPAWN_VELOCITY;
+  QColor color = QColor((int)(255 * heat_value), 255, (int)(255 * (1 - heat_value)));
   setBrush(color);
 
   setRect(0, 0, molecule_->radius_ * 2, molecule_->radius_ * 2);
@@ -15,5 +18,9 @@ void ParticleImage::advance(int phase) {
 
   if (phase) {
     setPos(molecule_->position_.x_ - molecule_->radius_, molecule_->position_.y_ - molecule_->radius_);
+    double heat_value = (molecule_->velocity_.Length()) / MAX_SPAWN_VELOCITY;
+    heat_value = heat_value > 1 ? 1 : heat_value;
+    QColor color = QColor((int)(255 * heat_value), 0, (int)(255 * (1 - heat_value)));
+    setBrush(color);
   }
 }
